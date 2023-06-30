5608160ae836e6aab2ce33633ad4525d34b4cf71
fafadevhub <fabriciopereiraleal2021@gmail.com>
2023-06-30 12:46:35 -0300

    Mais fundamentos:  Tipagem Fraca, Variáveis e Constantes

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,13 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40 //a variavel let não aceita redeclaração... prefira usar a variável let
+
+console.log(a, b)
+
+const c = 5
+//c = 50   geraria um erro, por conta que a variavel const é constante e não aceita redeclarar.
+
+
+
