0975a1e0f7bf3794efeb385e86e00eff7e88d444
fafadevhub <fabriciopereiraleal2021@gmail.com>
2023-07-05 13:20:27 -0300

    adicionando hoisting

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+console.log("a = ", a)
+var a = 2 // faz hoisting
+console.log("a = ", a)
+
+console.log("b = ", b)
+let b = 5 //não faz hoisting
+console.log("b = ", b)
+
+//hoisting sobe a variável para o topo
