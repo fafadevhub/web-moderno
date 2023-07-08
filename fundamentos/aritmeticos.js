e0b6625c9b5474c0a4e5cfe3ec7988de67229a7d
fafadevhub <fabriciopereiraleal2021@gmail.com>
2023-07-08 12:26:04 -0300

    adicionando aritmeticos, relacionais, ternarios, e unarios

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [5 , 10 , 20 , 40]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo)
